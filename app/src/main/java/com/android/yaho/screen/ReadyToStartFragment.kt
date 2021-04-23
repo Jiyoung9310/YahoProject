d8ed324a729a174e5a91bba57e5669e87c7fc1bf
jiyoung <ji9310@naver.com>
2021-04-23 14:02:30 +0900

    내 위치 마커 추가

--- app/src/main/java/com/android/yaho/screen/ReadyToStartFragment.kt
+++ app/src/main/java/com/android/yaho/screen/ReadyToStartFragment.kt
@@ -1,5 +1,6 @@
 package com.android.yaho.screen
 
+import android.graphics.PointF
 import android.os.Bundle
 import android.view.View
 import com.android.yaho.R
@@ -11,7 +12,10 @@ import com.android.yaho.screen.ReadyActivity.Companion.SCREEN_NEAR_MOUNTAIN
 import com.android.yaho.viewmodel.ReadyViewModel
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
-import com.naver.maps.map.*
+import com.naver.maps.map.CameraUpdate
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.overlay.OverlayImage
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class ReadyToStartFragment: BindingFragment<FragmentReadyToStartBinding>(FragmentReadyToStartBinding::inflate),
@@ -33,7 +37,7 @@ class ReadyToStartFragment: BindingFragment<FragmentReadyToStartBinding>(Fragmen
     }
 
     private fun initView() {
-        binding.tvNearTitle.text = getString(R.string.ready_to_start_title, mountainData?.name)
+        binding.tvNearTitle.text = getString(R.string.ready_to_start_title, mountainData.name)
     }
 
     override fun onMapReady(naverMap: NaverMap) {
@@ -44,6 +48,7 @@ class ReadyToStartFragment: BindingFragment<FragmentReadyToStartBinding>(Fragmen
             setLayerGroupEnabled(NaverMap.LAYER_GROUP_MOUNTAIN, true)
             minZoom = 4.0
             maxZoom = 13.0
+            setContentPadding(100, 100, 100, 100)
             uiSettings.apply {
                 isTiltGesturesEnabled = false
                 isRotateGesturesEnabled = false
@@ -53,7 +58,6 @@ class ReadyToStartFragment: BindingFragment<FragmentReadyToStartBinding>(Fragmen
                 isCompassEnabled = false
                 isScaleBarEnabled = false
             }
-            setContentPadding(50, 50, 50, 50)
         }
 
         val location = viewModel.currentLocation
@@ -65,6 +69,15 @@ class ReadyToStartFragment: BindingFragment<FragmentReadyToStartBinding>(Fragmen
                 )
             )
             naverMap.moveCamera(cameraUpdate)
+
+            naverMap.locationOverlay.apply {
+                isVisible = true
+                position = LatLng(it.latitude, it.longitude)
+                icon = OverlayImage.fromResource(R.drawable.ic_map_location)
+                anchor = PointF(0.5f, 0f)
+                subIcon = OverlayImage.fromResource(R.drawable.ic_marker_go)
+                subAnchor = PointF(0.5f, 1f)
+            }
         }
 
     }
