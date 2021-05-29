7b75888f5b6d2680d4cb4551405adb8b95a0783b
jiyoung <ji9310@naver.com>
2021-05-29 15:57:24 +0900

    등산 기록 시 네트워크 상태 확인

--- app/src/main/java/com/android/yaho/screen/ClimbingDoneActivity.kt
+++ app/src/main/java/com/android/yaho/screen/ClimbingDoneActivity.kt
@@ -1,32 +1,52 @@
 package com.android.yaho.screen
 
 import android.animation.Animator
-import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Network
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.android.yaho.R
 import com.android.yaho.base.BindingActivity
 import com.android.yaho.databinding.ActivityClimbingDoneBinding
-import com.android.yaho.repository.ClimbingResult
+import com.android.yaho.ui.ClimbingSaveDialog
 import com.android.yaho.viewmodel.ClimbingDoneViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class ClimbingDoneActivity : BindingActivity<ActivityClimbingDoneBinding>(ActivityClimbingDoneBinding::inflate) {
+class ClimbingDoneActivity : BindingActivity<ActivityClimbingDoneBinding>(
+    ActivityClimbingDoneBinding::inflate
+) {
     private val TAG = this::class.java.simpleName
 
     private val viewModel by viewModel<ClimbingDoneViewModel>()
+    private lateinit var connectivityManager : ConnectivityManager
+    private var dialog : ClimbingSaveDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        connectivityManager = getSystemService(ConnectivityManager::class.java)
+        dialog = ClimbingSaveDialog(this)
+        dialog?.show()
 
         initView()
         initObserve()
     }
     private fun initView() {
-        binding.lottieDone.addAnimatorListener(object: Animator.AnimatorListener {
-            override fun onAnimationStart(p0: Animator?) {
+
+        connectivityManager.registerDefaultNetworkCallback(object :
+            ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                Log.e(TAG, "The default network is now: " + network)
                 viewModel.saveClimbData()
+                dialog?.dismiss()
+                binding.lottieDone.playAnimation()
+            }
+        })
+
+        binding.lottieDone.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator?) {
+
             }
 
             override fun onAnimationEnd(p0: Animator?) {
