292d8f7856975f632fdcbc62645e56b5a4778686
jiyoung <ji9310@naver.com>
2021-05-31 18:47:04 +0900

    위치 권한 안내 팝업 추가

---
+++ app/src/main/java/com/climbing/yaho/ui/LocationPermissionDialog.kt
@@ -0,0 +1,37 @@
+package com.climbing.yaho.ui
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatDialog
+import com.climbing.yaho.R
+import com.climbing.yaho.dp
+
+class LocationPermissionDialog(context: Context) : AppCompatDialog(context) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dialog_location_permission)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val metrics = DisplayMetrics()
+        val wm = context?.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        wm.defaultDisplay.getMetrics(metrics)
+        val dialogWidth = (metrics.widthPixels - 24.dp * 2)
+
+        window?.apply {
+            setLayout(dialogWidth, ViewGroup.LayoutParams.WRAP_CONTENT)
+            setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            setCanceledOnTouchOutside(false)
+        }
+    }
+
+}
