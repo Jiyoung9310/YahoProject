e595d28a1a32a6d7faac0bb8b2e76b6b0c41c4f7
jiyoung <ji9310@naver.com>
2022-04-03 16:28:53 +0900

    팝업 띄우지않는 로직

--- app/src/main/java/com/climbing/yaho/ui/AdRemovePopup.kt
+++ app/src/main/java/com/climbing/yaho/ui/AdRemovePopup.kt
@@ -2,6 +2,7 @@ package com.climbing.yaho.ui
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -35,12 +36,19 @@ class AdRemovePopup(private val onClickRemoveAds : () -> Unit) : BottomSheetDial
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 //        if(yahoPreference.isSubscribing) return
-        if(TimeUnit.MILLISECONDS.toDays(yahoPreference.noMoreAdsPopupToday) == TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis())) dialog?.dismiss()
 
         // 팝업 생성 시 전체화면으로 띄우기
         val bottomSheet = dialog?.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
         val behavior = BottomSheetBehavior.from<View>(bottomSheet!!)
-        behavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        val oldDay = TimeUnit.MILLISECONDS.toDays(yahoPreference.noMoreAdsPopupToday)
+        val today = TimeUnit.MILLISECONDS.toDays(System.currentTimeMillis())
+        if(yahoPreference.isSubscribing || oldDay == today) {
+            this.dismiss()
+        } else {
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+
 
         // 드래그해도 팝업이 종료되지 않도록
         behavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
@@ -58,10 +66,12 @@ class AdRemovePopup(private val onClickRemoveAds : () -> Unit) : BottomSheetDial
                 behavior.state = BottomSheetBehavior.STATE_HIDDEN
             }
             btnNoMoreToday.setOnClickListener {
+                Log.d("AdRemovePopup", "No more popup today")
                 behavior.state = BottomSheetBehavior.STATE_HIDDEN
                 yahoPreference.noMoreAdsPopupToday = System.currentTimeMillis()
             }
             btnRemoveAds.setOnClickListener {
+                Log.d("AdRemovePopup", "click remove ads")
                 behavior.state = BottomSheetBehavior.STATE_HIDDEN
                 onClickRemoveAds.invoke()
             }
